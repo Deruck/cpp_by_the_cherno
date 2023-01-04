d15a4aa1a77cb4c982430312214129f7f47370a2
deruck <815754845@qq.com>
2023-01-04 17:06:30 +0800

    update: cpt07

---
+++ cpt07_how_to_debug/Main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "Log.h"
+
+int main() 
+{
+    int a = 8;
+    a++;
+    const char *string = "Hello";
+    for (int i = 0; i < 5; i++)
+    {
+        const char c = string[i];
+        std::cout << c << std::endl;
+    }
+    log("Hello world!");
+    std::cin.get();
+}
