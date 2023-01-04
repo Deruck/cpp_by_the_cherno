d23cffaf4afb920a34c30963b9337053b503c5cc
deruck <815754845@qq.com>
2023-01-04 12:24:15 +0800

    update: cpt03

--- cpt02_how_the_cpp_compiler_works/compile.sh
+++ cpt02_how_the_cpp_compiler_works/compile.sh
@@ -1,5 +1,5 @@
-dir=$(dirname $0)
-g++ -E $dir/Math.cpp -o $dir/Math.ii  # 源码 -预处理-> 预处理源码
-g++ -S $dir/Math.ii -o $dir/Math.s  # 预处理源码 -编译-> 汇编代码
-g++ -c $dir/Math.s -o $dir/Math.o  # 汇编代码 -汇编-> 二进制机器码
-hexdump $dir/Math.o > $dir/Math.o.txt
+cd $(dirname $0)
+g++ -E ./Math.cpp -o ./Math.ii  # 源码 -预处理-> 预处理源码
+g++ -S ./Math.ii -o ./Math.s  # 预处理源码 -编译-> 汇编代码
+g++ -c ./Math.s -o ./Math.o  # 汇编代码 -汇编-> 二进制机器码
+hexdump ./Math.o > ./Math.o.txt  # 查看二进制码
